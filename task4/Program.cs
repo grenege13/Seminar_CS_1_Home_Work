adac8aa4e1c7dc2e1632236ee3e4da297da2928a
Ilya Fedorov <grenege13@gmail.com>
2022-08-05 18:05:31 +0300

    task4 begening

--- task4/Program.cs
+++ task4/Program.cs
@@ -4,3 +4,23 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Введите первое число :");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число :");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите третье число :");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+double numMax = 0;
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+         numMax = num1;
+         Console.WriteLine("Число " + numMax + " больше");
+    }
+    
+}
